05a12b1ec45e37ea8fba097955f3bac1d4693a64
Gabriel Lesperance <gabriel@wavo.me>
2011-09-22 19:01:14 -0400

    Now nodejs + npm installs properly.

--- manifests/npm.pp
+++ manifests/npm.pp
@@ -3,7 +3,7 @@ class nodejs::npm($user) {
   $NPM_INSTALL_DIR = '/tmp'
 
   exec { 'npm-install':
-      command => 'curl http://npmjs.org/install.sh | clean=yes sh'
+      command => 'bash curl http://npmjs.org/install.sh | clean=yes sh'
     , cwd     => $NPM_INSTALL_DIR
     , path    => ["/usr/bin/", "/bin"]
   }
