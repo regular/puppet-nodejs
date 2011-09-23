e41aced3d46e1c687b19d50a7842bf9e2ad1d253
Gabriel Lesperance <gabriel@wavo.me>
2011-09-23 14:06:26 -0400

    Manual npm git pull

--- manifests/npm.pp
+++ manifests/npm.pp
@@ -1,25 +1,21 @@
 class nodejs::npm($user) {
 
-  $npm_path = "/opt/npm-src"
+  $NPM_TMP_PATH = "/tmp/npm"
 
   package { "npm-git-dep":
       name    => 'git'
     , ensure  => "installed"
   }
   
-  vcsrepo { $npm_path:
-    ensure  => present,
-    provider => git,
-    source => "http://github.com/isaacs/npm.git",
-    require => [Exec["install_node"], Package["npm-git-dep"]], 
-    revision => "HEAD"
+  exec { 'npm-git-clone':
+      command => "git clone ${NPM_REPO} ${NPM_TMP_PATH}"
+    , path    => ['/usr/bin']
   }
-
   
   exec { "make_npm":
-    cwd => "$npm_path",
+    cwd => $NPM_TMP_PATH,
     command => "make install",
-    require => Vcsrepo[$npm_path],
+    require => Exec['npm-git-clone'],
     creates => "/usr/local/bin/npm",
     timeout => 0,
     path    => ["/usr/bin/","/bin/", "/opt/node/bin"],
